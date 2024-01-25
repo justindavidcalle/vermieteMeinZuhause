261285f4923c268bee8b9dccad2c76ffe0db7fe7
justindavidcalle <justincalle@outlook.de>
2024-01-25 22:47:08 +0100

    Added Home and About page

--- vermieteMeinZuhause/src/sites/About.jsx
+++ vermieteMeinZuhause/src/sites/About.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import justin from '../assets/JustinCalle.jpg'
+import '../css/About.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about-container'>
+      <div className='black-container'>
+        <div className='content-container'>
+          <div className='welcome-container'>
+            <h1>Willkommen auf meiner Website!</h1>
+            <hr className='line' />
+            <p>
+              Mein Name ist Justin Calle und dies ist mein Projekt zu: "Vermiete mein Zuhause".
+              Diese App ist mit Vite, Express und Stripe gebaut worden. Auftrag ist es,
+              Unterkunftsmöglichkeiten anzubieten und diese mit einem Payment-Provider zu 
+              realisieren. Hierbei habe ich mir Stripe zur Hilfe genutzt.
+              Eine Unterkunft wird nach dem zahlen in die Datenbank, welche eine MongoDB ist auf Atlas,
+              gespeichert mit dem Login, welches mit ReCAPTCHA geschützt ist. 
+            </p>
+          </div>
+          <div className='contact-container'>
+            <h1>Kontaktdaten</h1>
+            <hr className='line' />
+            <p>Name: Justin Calle</p>
+            <p>Schulauftrag von: Berufsbildungsschule Winterthur / BBW</p>
+            <p>Abgabe am: 25.01.2024</p>
+            <p>Github-Repository: <a href="https://github.com/justindavidcalle/vermieteMeinZuhause">github.com/justindavidcalle/vermieteMeinZuhause</a></p>
+          </div>
+        </div>
+        <div className='image-container'>
+          <img src={justin} alt="Justin Calle" id='justinCalle' />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- vermieteMeinZuhause/src/sites/Home.jsx
+++ vermieteMeinZuhause/src/sites/Home.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import '../css/Home.css'; // Import the CSS file for styling
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  useEffect(() => {
+    // JavaScript code for moving objects
+    const moveObjects = () => {
+      const objects = document.querySelectorAll('.moving-object');
+      objects.forEach((object, index) => {
+        // Randomize animation duration and delay for each object
+        const duration = 10; // Random duration between 3 and 8 seconds
+        const delay = Math.random() * 2; // Random delay between 0 and 2 seconds
 
-export default Home
+        // Apply animation properties
+        object.style.animationDuration = `${duration}s`;
+        object.style.animationDelay = `${delay}s`;
+        object.style.animationIterationCount = 'infinite';
+      });
+    };
+
+    moveObjects();
+  }, []);
+
+  return (
+    <>
+    <h1>Vermiete dein Zuhause!</h1>
+    <div className='animation'>
+      <div class="wrapper">
+            <div class="candles">
+              <div class="light__wave"></div>
+              <div class="candle1">
+                <div class="candle1__body">
+                  <div class="candle1__eyes">
+                    <span class="candle1__eyes-one"></span>
+                    <span class="candle1__eyes-two"></span>
+                  </div>
+                  <div class="candle1__mouth"></div>
+                </div>
+                <div class="candle1__stick"></div>
+              </div>
+              
+              <div class="candle2">
+                <div class="candle2__body">
+                  <div class="candle2__eyes">
+                    <div class="candle2__eyes-one"></div>
+                    <div class="candle2__eyes-two"></div>
+                  </div>
+                </div>
+                <div class="candle2__stick"></div>
+              </div>
+              <div class="candle2__fire"></div>
+              <div class="sparkles-one"></div>
+              <div class="sparkles-two"></div>
+              <div class="candle__smoke-one">
+
+              </div>
+              <div class="candle__smoke-two">
+
+              </div>
+              
+            </div>
+            <div class="floor">
+            </div>
+            
+
+          </div>
+    </div>
+  
+    </>
+          
+  );
+};
+
+export default Home;
